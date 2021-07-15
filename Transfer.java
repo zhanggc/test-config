512a05fd49059010c1cb3e33ad518645400886f5
zhanggc <zhanggc.china@gmail.com>
2021-07-15 21:30:57 +0800

    Update Transfer.java

--- Transfer.java
+++ Transfer.java
@@ -17,39 +17,44 @@ import java.util.concurrent.Future;
 import com.alibaba.fastjson.JSON;
 
 public class Transfer {
-    static String basePath = "D:\\xx\\修改\\";
-    static String inputFileName = "xx.xmind";
-    static String outputFileName = "xx-transfer.xmind";
+    static String basePath = "D:\\xxx\\修改\\";
+    static String inputFileName = "xxx.xmind";
+    static String outputFileName = "xxx-transfer.xmind";
 
     /**
-     * 每个块存储元素个数(每块2M)
+     * 合并Json个数
      */
-    static int numPerBlock = (1 << 23) / 24;
+    static final int NUM_MERGE_JSON = 40;
+
+    /**
+     * 每个Json包含元素个数(每块512KB)，转JSON特别耗时间，每次转个数尽量少
+     */
+    static int NUM_TO_JSON = (1 << 23) / 24 / 40;
+
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        transferToTxt();
-//        transferToScr();
+//        transferToTxt();
+        transferToScr();
     }
 
     public static void transferToTxt() throws IOException, ExecutionException, InterruptedException {
         long start = System.currentTimeMillis();
         InputStream input = new FileInputStream(basePath + inputFileName);
-        OutputStream output = new FileOutputStream(basePath + "dst.txt");
         int n;
         List<Integer> list = new LinkedList();
         while ((n = input.read()) != -1) {
             list.add(n);
         }
         int len = list.size();
-        if (len > numPerBlock) {
-            int all = (int) Math.ceil((double) len / (double) numPerBlock);
-            ExecutorService executor = Executors.newFixedThreadPool(all);
+        if (len > NUM_TO_JSON) {
+            int all = (int) Math.ceil((double) len / (double) NUM_TO_JSON);
+            ExecutorService executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
             List<Future<String>> futures = new ArrayList<>();
             int num = 0;
             for (int i = 0; i < len; ) {
                 List subList;
                 int current = i;
-                i += numPerBlock;
+                i += NUM_TO_JSON;
                 int end = i;
                 if (end >= len - 1) {
                     subList = list.subList(current, len);
@@ -75,23 +80,47 @@ public class Transfer {
                 num++;
             }
 
+            List<String> blocks = new ArrayList<>();
+            StringBuilder merge = new StringBuilder();
             for (int i = 0; i < futures.size(); i++) {
                 System.out.println("开始处理:" + (i + 1) + "/" + all);
-                output.write(futures.get(i).get().getBytes("UTF-8"));
-                output.write("||".getBytes("UTF-8"));
-                output.flush();
-                System.out.println("处理完:" + (i + 1) + "/" + all);
+                merge.append(futures.get(i).get());
+                if ((i + 1) % NUM_MERGE_JSON == 0) {
+                    blocks.add(merge.toString().replaceAll("]\\[", ","));
+                    merge = new StringBuilder();
+                    System.out.println("处理完:" + (i + 1) + "/" + all);
+                    continue;
+                }
+
+                //最后一个
+                if (futures.size() - 1 == i) {
+                    blocks.add(merge.toString().replaceAll("]\\[", ","));
+                    merge = new StringBuilder();
+                    System.out.println("处理完:" + (i + 1) + "/" + all);
+                    continue;
+                }
             }
             executor.shutdown();
+            for (int i = 0; i < blocks.size(); i++) {
+                int index = i + 1;
+                System.out.println("合并开始处理:" + index + "/" + blocks.size());
+                OutputStream output = new FileOutputStream(basePath + "dst-" + index + ".txt");
+                output.write(blocks.get(i).getBytes("UTF-8"));
+                output.flush();
+                output.close();
+                System.out.println("合并处理完:" + index + "/" + blocks.size());
+            }
         } else {
             System.out.println("开始处理:1/1");
             String str = JSON.toJSONString(list);
+            OutputStream output = new FileOutputStream(basePath + "dst.txt");
             output.write(str.getBytes("UTF-8"));
+            output.flush();
+            output.close();
             System.out.println("处理完:1/1");
 
         }
 
-        output.close();
         input.close();
         System.out.println("花费时间: " + (System.currentTimeMillis() - start) / 1000 + "秒");
     }
@@ -99,7 +128,9 @@ public class Transfer {
 
     public static void transferToScr() throws IOException {
         OutputStream output = new FileOutputStream(basePath + outputFileName);
-        List<Integer> list = JSON.parseObject(readAsString1(), List.class);
+        String jsonArrayStr = readAsString1();
+        jsonArrayStr = jsonArrayStr.replaceAll("]\\[", ",");
+        List<Integer> list = JSON.parseObject(jsonArrayStr, List.class);
         list.forEach(i -> {
             try {
                 output.write(i);
