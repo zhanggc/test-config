2d606f1e579075fe0456323cfa6dc8b389b34bff
zhanggc <zhanggc.china@gmail.com>
2021-08-30 22:25:13 +0800

    Update Transfer1.java

--- Transfer1.java
+++ Transfer1.java
@@ -25,22 +25,22 @@ import java.util.concurrent.atomic.AtomicInteger;
 import com.alibaba.fastjson.JSON;
 
 public class Transfer {
-    static final String basePath = "C:\\Users\\zwx934029\\Desktop\\classes\\dest";
-    static final String inputFileName = "1.rar";
-    static final String outputFileName = "1-transfer.rar";
+    static final String basePath = xxx";
+    static final String inputFileName = "1.zip";
+    static final String outputFileName = "1-transfer.zip";
     static final String charMapFileName = "char-map.txt";
 
     /**
      * 块包含元素个数(1M)
      */
-    static final int NUM_TO_BLOCK = (1 << 19);
+    static final int NUM_TO_BLOCK = (1 << 20);
 
     //    static final String ONE_BYTE_CHAR_STR = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 //    static final char[] ONE_BYTE_CHARS = new char[ONE_BYTE_CHAR_STR.length()];
 //    static final String TWO_BYTE_CHAR_STR = "¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽžſƀƁƂƃƄƅƆƇƈƉƊƋƌƍƎƏƐƑƒƓƔƕƖƗƘƙƚƛƜƝƞƟƠơƢƣƤƥƦƧƨƩƪƫƬƭƮƯưƱƲƳƴƵƶƷƸƹƺƻƼƽƾƿǀǁǂǃǄǅǆǇǈǉǊǋǌǍǎǏǐǑǒǓǔǕǖǗǘǙǚǛǜǝǞǟǠǡǢǣǤǥǦǧǨǩǪǫǬǭǮǯǰǱǲǳǴǵǶǷǸǹǺǻǼǽǾǿ";
 //    static final char[] TWO_BYTE_CHARS = new char[TWO_BYTE_CHAR_STR.length()];
     static final Character[] CHAR_CAHCE = new Character[256];
-    static final List<Character> CHAR_TRANSFER = new ArrayList<>(256);
+    static final List<String> CHAR_TRANSFER = new ArrayList<>(256);
     static final Integer[] INTEGER_CAHCE = new Integer[256];
 
     static {
@@ -48,13 +48,26 @@ public class Transfer {
 //        TWO_BYTE_CHAR_STR.getChars(0, TWO_BYTE_CHAR_STR.length(), TWO_BYTE_CHARS, 0);
         for (char i = 0; i < CHAR_CAHCE.length; i++) {
             CHAR_CAHCE[i] = Character.valueOf(i);
-                if(i > 13 && i != 32 && i != 160){
-                    CHAR_TRANSFER.add(CHAR_CAHCE[i]);
-                }
+
         }
-        for (char i = 256; i < 356; i++) {
-            CHAR_TRANSFER.add(Character.valueOf(i));
+
+        for (char i = 0; i <= 127; i++) {
+            //33、35-37、39-43、45-59、61、63-91、93-127
+            if((35<=i && i<=37)|| (39<=i && i<=43)|| (45<=i && i<=59)|| (63<=i && i<=91)|| (93<=i && i<=127)){
+                CHAR_TRANSFER.add(String.valueOf(i));
+            }
         }
+
+        char s_33 = 33;
+        char s_61 = 61;
+        List<String> newList = new ArrayList<>(256);
+        for(String str:CHAR_TRANSFER){
+            newList.add(str+String.valueOf(s_33));
+        }
+        for(String str:CHAR_TRANSFER){
+            newList.add(str+String.valueOf(s_61));
+        }
+        CHAR_TRANSFER.addAll(newList);
         for (char i = 0; i < INTEGER_CAHCE.length; i++) {
             INTEGER_CAHCE[i] = Integer.valueOf(i);
         }
@@ -62,8 +75,33 @@ public class Transfer {
 
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
-        transferToTxt();
-//        transferToScr();
+//        transferToTxt();
+        transferToScr();
+//        output();
+//        output1();
+    }
+
+
+    public static void output1() throws IOException {
+        List<Character> list = new LinkedList();
+        for(char ca:CHAR_CAHCE){
+            if(ca < 14 || ca == 32|| ca == 160){
+                continue;
+            }
+
+            list.add(ca);
+        }
+        for(Character _char:list){
+            List<Character> list1 = new ArrayList<>(10000);
+            for(int i=0;i<1100000;i++){
+                list1.add(_char);
+            }
+            OutputStream output = new FileOutputStream(basePath + System.getProperty("file.separator") + "Char-"+(int)_char+".txt");
+            String block = transferBlockToStr(list1);
+            output.write(block.getBytes("UTF-8"));
+            output.flush();
+            output.close();
+        }
     }
 
 
@@ -91,7 +129,7 @@ public class Transfer {
         InputStream input = new FileInputStream(basePath + System.getProperty("file.separator") + inputFileName);
         int n;
         List<Byte> numList = new LinkedList();
-        List<Character> charList = new LinkedList();
+        List<String> charList = new LinkedList();
         NumFequency fequency = new NumFequency();
         StringBuilder trace = new StringBuilder();
         byte[] bytes = new byte[8192];
@@ -209,17 +247,39 @@ public class Transfer {
         StringBuilder trace = new StringBuilder();
         byte[] batch = new byte[8192];
         short batchIndex = 0;
+        char s_33 = 33;
+        char s_61 = 61;
+        char prevChar = 0;
         for (int i = 0; i < charArray.length; i++) {
-            Byte index = fequency.getNum(charArray[i]);
-            batch[batchIndex] = index;
-            batchIndex++;
+            char addChar = charArray[i];
+            if(s_33 == addChar || s_61 == addChar){
+                String num2Char = String.valueOf(prevChar)+String.valueOf(addChar);
+                batch[batchIndex-1] = fequency.getNum(num2Char);;
+                if (batch.length <= batchIndex ) {
+                    output.write(batch);
+                    batchIndex = 0;
+                }else if (i + 1 >= charArray.length) {
+                    output.write(batch, 0, batchIndex);
+                }
+            }else{
+                Byte index = fequency.getNum(String.valueOf(charArray[i]));
+                batch[batchIndex] = index;
+                batchIndex++;
 //            trace.append(index).append(",");
-            if (batch.length <= batchIndex) {
-                output.write(batch);
-                batchIndex = 0;
-            } else if (i + 1 == charArray.length) {
-                output.write(batch, 0, batchIndex);
+                if (batch.length <= batchIndex ) {
+                    if(i<charArray.length-1){ //不是最后一个
+                        if(s_33 == charArray[i+1] || s_61 == charArray[i+1]){
+                            prevChar = addChar;
+                            continue;
+                        }
+                    }
+                    output.write(batch);
+                    batchIndex = 0;
+                } else if (i + 1 >= charArray.length) {
+                    output.write(batch, 0, batchIndex);
+                }
             }
+            prevChar = addChar;
         }
         output.close();
 //        System.out.println("trace write bit:" + trace.toString());
@@ -283,7 +343,7 @@ public class Transfer {
      */
     static class NumFequency {
         Map<Byte, Integer> numFeq = new HashMap<>();
-        Map<Byte, Character> numToChar = new HashMap<>();
+        Map<Byte, String> numToChar = new HashMap<>();
         Map<String, Byte> charToNum = new HashMap<>();
         Set<NumFeqItem> feqSet = new TreeSet<>();
 
@@ -338,7 +398,7 @@ public class Transfer {
             StringBuilder trace = new StringBuilder();
             feqSet.forEach(item -> {
                 int i1 = i.get();
-                Character _char = CHAR_TRANSFER.get(i1);
+                String _char = CHAR_TRANSFER.get(i1);
                 numToChar.put(item.num, _char);
                 charToNum.put(String.valueOf(_char).intern(), item.num);
                 i.getAndIncrement();
@@ -351,7 +411,7 @@ public class Transfer {
             System.out.println("trace num feq desc:" + trace.toString());
         }
 
-        public Character getChar(Byte num) {
+        public String getChar(Byte num) {
             return numToChar.get(num);
         }
 
@@ -388,8 +448,8 @@ public class Transfer {
             System.out.println("结束读取开始读取charMap文件文件:" + charMapFileName + ".txt");
         }
 
-        public Byte getNum(Character character) {
-            return charToNum.get(character.toString());
+        public Byte getNum(String str) {
+            return charToNum.get(str);
         }
     }
 }
